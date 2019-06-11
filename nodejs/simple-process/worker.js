de627b9356220aa4276b95d8ee37d65afd3dc456
qufei <quzhenfei@boluome.com>
2019-06-11 18:29:24 +0800

    守护进程

---
+++ nodejs/simple-process/worker.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+	res.writeHead(200, {
+		'Content-Type': 'text/plan'
+	});
+
+	res.end('I am worker, pid: ' + process.pid + ', ppid: ' + process.ppid);
+
+	throw new Error('worker process exception!');
+});
+
+let worker;
+process.title = 'node-worker'
+process.on('message', function (message, sendHandle) {
+	if (message === 'server') {
+		worker = sendHandle;
+		worker.on('connection', function(socket) {
+			server.emit('connection', socket);
+		});
+	}
+});
+
+process.on('uncaughtException', function (err) {
+	console.log(err);
+
+	process.send({act: 'suicide'});
+
+	worker.close(function () {
+		process.exit(1);
+	})
+})
